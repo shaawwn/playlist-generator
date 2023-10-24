992cad4225a945d53de8fbb1ee78665f1fb7a817
Shawn Lentz <shawn.lentz@gmail.com>
2023-10-24 09:46:04 -1000

    reworking login page

--- src/Login.js
+++ src/Login.js
@@ -5,12 +5,18 @@ import React from 'react';
 export default function Login({authUrl}) {
 
     return(
-        <div className="view">
-            <a href={authUrl}>
-                <div className="btn login--btn">
-                    <h1>Login with Spotify</h1>
-                </div>
-            </a>
+        <div className="view view__login">
+            <div className="login__banner">
+                <p>Playlist Generator</p>
+            </div>
+            <div className="login__intro">
+                <p>Generate playlists by choosing tracks or artists. You can choose from your top items, or you acn search for new ones.</p>
+                <a href={authUrl}>
+                    <div className="btn login--btn">
+                        <h1>Login with Spotify</h1>
+                    </div>
+                </a>  
+            </div>
         </div>
 
     )
