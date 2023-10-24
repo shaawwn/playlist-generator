61dfd45feb1d0a0465e23371d769f7fdc196298a
Shawn Lentz <shawn.lentz@gmail.com>
2023-10-24 11:41:59 -1000

    restyle login screen

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMusic, faHandPointer, faSpinner, faCheck } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function Login({authUrl}) {
@@ -7,13 +8,39 @@ export default function Login({authUrl}) {
     return(
         <div className="view view__login">
             <div className="login__banner">
-                <p>Playlist Generator</p>
+                <p className="login__banner__title">Playlist Generator</p>
+                <div className="music">
+                    <FontAwesomeIcon icon={faMusic} size="4x" />
+                    <FontAwesomeIcon icon={faMusic} size="4x" />
+                </div>
             </div>
+
             <div className="login__intro">
-                <p>Generate playlists by choosing tracks or artists. You can choose from your top items, or you acn search for new ones.</p>
+                <div className="login__intro__flowchart">
+                    <div className="login__intro__flowchart__item">
+                        <p>Choose Tracks and Artists</p>
+                        <div className="login__intro__item__wrapper">
+                            <FontAwesomeIcon icon={faHandPointer} size="2x" className="login__hand" />
+                            <div className="top-items__item animation__piece">
+                                <p>Freebird</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className="login__intro__flowchart__item">
+                        <p>Generate a playlist</p>
+                        <FontAwesomeIcon icon={faSpinner} size="2x" className="login__spinner"/>
+                    </div>
+
+                    <div className="login__intro__flowchart__item">
+                        <p>Play in browser or save playlist to Spotify!</p>
+                        <FontAwesomeIcon icon={faCheck} size="2x" className="login__check"/>
+                    </div>
+                </div>
                 <a href={authUrl}>
-                    <div className="btn login--btn">
-                        <h1>Login with Spotify</h1>
+                    <div className="btn login__btn">
+                        <h1>Get Started</h1>
                     </div>
                 </a>  
             </div>
